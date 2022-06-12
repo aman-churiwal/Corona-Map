856d268ed9c4841915693bf0f3a6207bd1a39aca
Aman <amanchuriwal22@gmail.com>
2022-06-12 11:47:40 +0530

    added marker

--- index.js
+++ index.js
@@ -7,8 +7,21 @@ function updateMap() {
                 latitude = element.latitude;
                 longitude = element.longitude;
 
+                cases = element.infected;
+                if (cases > 255) {
+                    color = "rgb(255,0,0)"
+                }
+                else {
+                    color = `rgb(${cases},0,0)`
+                }
                 // Mark on the Map
-            })
+                new mapboxgl.Marker({
+                    draggable: false,
+                    color: color
+                })
+                    .setLngLat([longitude, latitude])
+                    .addTo(map);
+            });
         })
 }
 
